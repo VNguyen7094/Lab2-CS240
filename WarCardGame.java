2702ec2836d03e389ebe1efa25ce52c5c37e0b42
VNguyen7094 <168499240+VNguyen7094@users.noreply.github.com>
2025-01-28 08:41:38 -0800

    Add files via upload

---
+++ WarCardGame.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class WarCardGame {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Deck deck = new Deck();
+        deck.shuffleDeck();
+        
+        // Split the deck 
+        Queue<Card> playerA = new LinkedList<>();
+        Queue<Card> playerB = new LinkedList<>();       
+        // Deal cards
+        for (int i = 0; i < 26; i++) {
+            playerA.add(deck.drawCard());
+            playerB.add(deck.drawCard());
+        }
+
+        while (!playerA.isEmpty() && !playerB.isEmpty()) {
+            System.out.println("Press to play!!!");
+            in.nextLine();
+            
+            // Each player draws a card
+            Card playerACard = playerA.poll();
+            Card playerBCard = playerB.poll();
+            
+            System.out.println("It's A turn: " + playerACard);
+            System.out.println("It's B turn: " + playerBCard);
+
+            // Compare the cards
+            if (playerACard.getValue() > playerBCard.getValue()) {
+                System.out.println("Player A wins this round!");
+                playerA.add(playerACard);
+                playerB.add(playerBCard);
+            } else if (playerBCard.getValue() > playerACard.getValue()) {
+                System.out.println("Player B wins this round!");
+                playerB.add(playerACard);
+                playerB.add(playerBCard);
+            } else {
+                System.out.println("It's a tie!");
+            
+            }
+
+            System.out.println("Player A has " + playerA.size() + " cards left.");
+            System.out.println("Player B has " + playerB.size() + " cards left.");
+        }
+
+        if (playerA.isEmpty()) {
+            System.out.println("Player A! You're win");
+        } else {
+            System.out.println("Player B! You're win");
+        }
+        in.close();
+    }
+}
